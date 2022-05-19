1cc71e6ccc4c664c975695153b1040464df3b2b8
Aaronjill <aaronjillundra@gmail.com>
2022-05-19 14:28:19 +0530

    change

--- ROLLERCOASTER HIEGHT REQ.py
+++ ROLLERCOASTER HIEGHT REQ.py
@@ -1,14 +1,22 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height>=120:
     print('You can ride the rollercoaster')
     age=int(input("Whats your age ?"))
     if age > 12:
         print("You have to pay $5. ")
+        bill = 5
     elif age >=18:
         print("You have to pay $7. ")
+        bill = 7
     else:
         print("You have to pay $12. ")
+        bill = 12
+    want_photo=input('do you want a photo? Y or N.')
+    if want_photo == 'Y':
+        bill += 3
+    print('your final bill is',bill)
 else:
     print('Too small for this shit')
