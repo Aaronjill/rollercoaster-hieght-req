e1c6e3e26a1c2d7411c72f0f6e9b4ffd23107a02
Aaronjill <aaronjillundra@gmail.com>
2022-05-19 14:52:47 +0530

    midlife crisis

--- ROLLERCOASTER HIEGHT REQ.py
+++ ROLLERCOASTER HIEGHT REQ.py
@@ -5,12 +5,15 @@ bill = 0
 if height>=120:
     print('You can ride the rollercoaster')
     age=int(input("Whats your age ?"))
-    if age > 12:
+    if  age >=45 and age <=55:
+        bill += 0
+    elif age > 12:
         print("You have to pay $5. ")
         bill = 5
     elif age >=18:
         print("You have to pay $7. ")
         bill = 7
+    
     else:
         print("You have to pay $12. ")
         bill = 12
